331154b4c7c37877121645a71386e11edd078c9a
morcosshehata9 <marcosshehata7422@gmail.com>
2025-04-28 16:18:29 +0300

    Init project

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+
+    }
+}
